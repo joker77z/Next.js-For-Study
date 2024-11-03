699e087ae32c745373b39bdff01d0f23fe2cc89f
TaejoonPark <joker77z@naver.com>
2024-11-03 11:51:09 +0900

    feat: SEO 추가

--- onebite-next/section02/src/pages/book/[id].tsx
+++ onebite-next/section02/src/pages/book/[id].tsx
@@ -1,5 +1,6 @@
 import fetchOneBook from "@/lib/fetch-one-book";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import style from "./[id].module.css";
 
@@ -33,23 +34,42 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
 
 export default function BookDetailPage({ book }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
-  if (router.isFallback) return <div>로딩 중...</div>;
+  if (router.isFallback)
+    return (
+      <>
+        <Head>
+          <title>한입북스 - 검색결과</title>
+          <meta property="og:image" content="/thumbnail.png" /> {/* NOTE: 이미지 경로는 public 디렉토리 기준 */}
+          <meta property="og:title" content="한입북스 - 검색결과" />
+          <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요." />
+        </Head>
+        <div>로딩 중...</div>
+      </>
+    );
   if (!book) return "문제가 발생했습니다.";
 
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
-    <div className={style["top-container"]}>
-      <div style={{ backgroundImage: `url('${coverImgUrl}')` }} className={style.container}>
-        <img src={coverImgUrl} />
-      </div>
-      <div className={style.title}>{title}</div>
-      <div className={style.subTitle}>{subTitle}</div>
-      <div className={style.author}>
-        {author} | {publisher}
-      </div>
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={coverImgUrl} /> {/* NOTE: 이미지 경로는 public 디렉토리 기준 */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={style["top-container"]}>
+        <div style={{ backgroundImage: `url('${coverImgUrl}')` }} className={style.container}>
+          <img src={coverImgUrl} />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
 
-      <div className={style.description}>{description}</div>
-    </div>
+        <div className={style.description}>{description}</div>
+      </div>
+    </>
   );
 }
--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -3,6 +3,7 @@ import SearchableLayout from "@/components/searchable-layout/searchable-layout";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 import { InferGetStaticPropsType } from "next";
+import Head from "next/head";
 import { ReactNode } from "react";
 import styles from "./index.module.css";
 
@@ -19,20 +20,28 @@ export const getStaticProps = async () => {
 
 export default function Home({ allBooks, recoBooks }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={styles.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" /> {/* NOTE: 이미지 경로는 public 디렉토리 기준 */}
+        <meta property="og:title" content="한입북스 - 검색결과" />
+        <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요." />
+      </Head>
+      <div className={styles.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
